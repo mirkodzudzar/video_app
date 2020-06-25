9166b5c93771f2db18c6155e8651c88490e8290f
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-06-25 14:30:51 +0200

    No search results test

---
+++ tests/Controllers/Front/FrontControllerVideoTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Controllers\Front;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FrontControllerVideoTest extends WebTestCase
+{
+    public function testNoResults() {
+
+        $client = static::createClient();
+        $client->followRedirects();
+
+        $crawler = $client->request('GET', '/');
+
+        $form = $crawler->selectButton('Search video')->form([
+            'query' => 'aaa',
+        ]);
+        $crawler = $client->submit($form);
+
+        $this->assertContains('No results were found', $crawler->filter('h1')->text());
+    }
+
+}
