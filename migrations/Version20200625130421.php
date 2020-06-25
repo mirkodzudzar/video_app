7f5d91e70949c8337788b266ce992f5a00eca7db
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-06-25 15:19:00 +0200

    Login, logout, remember me setup

--- migrations/Version20200625130421.php
+++ migrations/Version20200625130421.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200625125253 extends AbstractMigration
+final class Version20200625130421 extends AbstractMigration
 {
     public function getDescription() : string
     {
