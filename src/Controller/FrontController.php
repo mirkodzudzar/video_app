4f23f5b6f3c4f0889c394c0ae153bbd736ad6823
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-06-26 11:45:49 +0200

    User registration

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Video;
+use App\Form\UserType;
 use App\Entity\Category;
 use App\Utils\CategoryTreeFrontPage;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class FrontController extends AbstractController
 {
@@ -78,9 +82,33 @@ class FrontController extends AbstractController
     /**
      * @Route("/register", name="register")
      */
-    public function register() {
+    public function register(Request $request, UserPasswordEncoderInterface $password_encoder) {
 
-        return $this->render('front/register.html.twig');
+        $user = new User;
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $user->setName($request->request->get('user')['name']);
+            $user->setLastName($request->request->get('user')['last_name']);
+            $user->setEmail($request->request->get('user')['email']);
+            $password = $request->request->get('user')['password']['first'];
+            $user->setPassword($password_encoder->encodePassword($user, $password));
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->loginUserAutomatically($user, $password);
+
+            return $this->redirectToRoute('admin_main_page');
+        }
+
+        return $this->render('front/register.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
@@ -91,6 +119,19 @@ class FrontController extends AbstractController
         return $this->render('front/login.html.twig', [
             'error' => $helper->getLastAuthenticationError(),
         ]);
+    }
+
+    private function loginUserAutomatically($user, $password) {
+
+        $token = new UsernamePasswordToken(
+            $user,
+            $password,
+            'main',
+            $user->getRoles()
+        );
+
+        $this->get('security.token_storage')->setToken($token);
+        $this->get('session')->set('_security_main', serialize($token));
     }
 
     /**
---
+++ src/Form/UserType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('last_name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Password',
+                    'attr' => [
+                        'class' => 'mb-3'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Repeat password',
+                    'attr' => [
+                        'class' => 'mb-3'
+                    ]
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Register'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
