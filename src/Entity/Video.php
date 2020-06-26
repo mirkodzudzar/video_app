9cf5ad764bd0d398059126e8e912bc4a6e8a16b2
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-06-26 13:22:28 +0200

    Creating Comment entity. Aditional fields for video

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Index as Index;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\VideoRepository;
@@ -38,6 +40,16 @@ class Video
      * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="videos")
      */
     private $category;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="video")
+     */
+    private $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -88,6 +100,37 @@ class Video
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setVideo($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getVideo() === $this) {
+                $comment->setVideo(null);
+            }
+        }
 
         return $this;
     }
