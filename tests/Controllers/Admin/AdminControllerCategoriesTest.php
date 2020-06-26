1b251f8044d3037978d953d468fde96e074f6e8b
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-06-26 16:27:10 +0200

    Functional testing

--- tests/Controllers/Admin/AdminControllerCategoriesTest.php
+++ tests/Controllers/Admin/AdminControllerCategoriesTest.php
@@ -4,33 +4,11 @@ namespace App\Tests\Controllers\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Entity\Category;
+use App\Tests\Rollback;
 
 class AdminControllerCategoriesTest extends WebTestCase
 {
-    public function setUp() {
-
-        // Super admin user
-        parent::setUp();
-        $this->client = static::createClient([], [
-            'PHP_AUTH_USER' => 'mirko@gmail.com',
-            'PHP_AUTH_PW' => 'mirko'
-        ]);
-        // Prevents from shutting down the kernel between test request and thus losing transactions.
-        $this->client->disableReboot();
-
-        $this->entityManager = $this->client->getContainer()->get('doctrine.orm.entity_manager');
-        $this->entityManager->beginTransaction();
-        $this->entityManager->getConnection()->setAutoCommit(false);
-    }
-
-    public function tearDown() {
-
-        parent::tearDown();
-        // This is used for rolling back DB data for not making any changes.
-        $this->entityManager->rollback();
-        $this->entityManager->close();
-        $this->entityManager = null; //prevent memory leaks
-    }
+    use Rollback;
 
     public function testTextOnPage() {
 
