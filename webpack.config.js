a1a1db7fed18e3dfe2331d19c14dcf150f75b850
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-06-29 12:14:02 +0200

    Customizing likes/dislikes - front

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,12 @@ Encore
     // public path used by the web server to access the output path
     .setPublicPath('/assets')
 
+    .addEntry('js/likes', './assets/js/likes.js')
+
     // .addEntry('app', './assets/js/app.js')
     .addStyleEntry('css/dashboard', ['./assets/css/dashboard.css'])
     .addStyleEntry('css/login', ['./assets/css/login.css'])
+    .addStyleEntry('css/likes', ['./assets/css/likes.css'])
 
 ;
 
