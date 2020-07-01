52255e2d860ffb4e7c07e7b36e9cdf8d213c9d09
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-07-01 14:29:32 +0200

    Delete video and video path

--- src/Controller/Admin/Superadmin/SuperAdminController.php
+++ src/Controller/Admin/Superadmin/SuperAdminController.php
@@ -56,6 +56,30 @@ class SuperAdminController extends AbstractController
     }
 
     /**
+     * @Route("/delete-video/{video}/{path}", name="delete_video", requirements={"path"=".+"})
+     */
+    public function deleteVideo(Video $video, $path, UploaderInterface $fileUploader) {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($video);
+        $entityManager->flush();
+
+        if ($fileUploader->delete($path)) {
+            $this->addFlash(
+                'success',
+                'The video was successfully deleted.'
+            );
+        } else {
+            $this->addFlash(
+                'danger',
+                'We were not able to delete, check the video.'
+            );
+        }
+
+        return $this->redirectToRoute('videos');
+    }
+
+    /**
      * @Route("/users", name="users")
      */
     public function users() {
