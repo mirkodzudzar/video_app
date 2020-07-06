9fce402c2a7c3de792a47fb429857d9f0f8732bb
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-07-06 12:27:53 +0200

    Functional tests - video deletion

--- migrations/Version20200706102644.php
+++ migrations/Version20200706102644.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200703122208 extends AbstractMigration
+final class Version20200706102644 extends AbstractMigration
 {
     public function getDescription() : string
     {
--- tests/Controllers/Admin/AdminControllerSecurityTest.php
+++ tests/Controllers/Admin/AdminControllerSecurityTest.php
@@ -29,6 +29,7 @@ class AdminControllerSecurityTest extends WebTestCase
         yield ['GET', '/admin/su/delete-category/1'];
         yield ['GET', '/admin/su/users'];
         yield ['GET', '/admin/su/upload-video-locally'];
+        yield ['GET', '/admin/su/upload-video-by-vimeo'];
     }
 
     // Super admin user
