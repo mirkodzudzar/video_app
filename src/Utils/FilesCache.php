8f624837ff5708f970c3d9f0b0415dbda195bbca
Mirko Dzudzar <mirko.dzudzar@fsdc.info>
2020-07-07 10:55:46 +0200

    Reseting git structure

--- src/Utils/FilesCache.php
+++ src/Utils/FilesCache.php
@@ -11,6 +11,28 @@ class FilesCache implements CacheInterface {
   public $cache;
   public function __construct() {
 
-    $this->cache = new TagAwareAdapter(new FilesystemAdapter());
+    // not working on free Heroku account:
+    // $this->cache =  new TagAwareAdapter(
+    // new FilesystemAdapter()
+    // );
+
+    $provider = new SQLite3Cache(new \SQLite3(__DIR__ . '/cache/data.db'), 'TableName');
+
+    $this->cache =  new TagAwareAdapter(
+
+        new DoctrineAdapter(
+
+            // a cache provider instance
+            $provider,
+
+            // a string prefixed to the keys of the items stored in this cache
+            $namespace = '',
+
+            // the default lifetime (in seconds) for cache items that do not define their
+            // own lifetime, with a value 0 causing items to be stored indefinitely (i.e.
+            // until the database table is truncated or its rows are otherwise deleted)
+            $defaultLifetime = 0
+        )
+    );
   }
 }
